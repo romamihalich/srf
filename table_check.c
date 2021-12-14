66c68587a3d564e57312c61878f0fb16e737f178
romamihalich <miheev.roma90@mail.ru>
2021-12-14 21:35:23 +0300

    Added table property checks

--- table_check.c
+++ table_check.c
@@ -57,3 +57,77 @@ bool isdistributive_right(int mult[N*N], int add[N*N]) {
 bool isdistributive(int mult[N*N], int add[N*N]) {
     return isdistributive_left(mult, add) && isdistributive_right(mult, add);
 }
+
+bool ismono(int mult[N*N], int add[N*N]) {
+    for (int i = 0; i < N*N; i++) {
+        if (mult[i] != add[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool isconst(int table[N*N]) {
+    for (int i = 0; i < N*N - 1; i++) {
+        if (table[i] != table[i + 1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool isneutral(int table[N*N], int inx) {
+    for (int i = 0; i < N; i++) {
+        if (table[inx*N + i] != i || table[i*N + inx] != i) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// -1 if not exist
+int neutral(int table[N*N]) {
+    for (int i = 0; i < N; i++) {
+        if (isneutral(table, i)) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+// -1 if not exist
+int zero(int mult[N*N], int add[N*N]) {
+    int add_neu = neutral(add);
+    if (add_neu == -1) {
+        return -1;
+    }
+    for (int i = 0; i < N; i++) {
+        if (mult[add_neu*N + i] != add_neu || mult[i*N + add_neu] != add_neu) {
+            return -1;
+        }
+    }
+    return add_neu;
+}
+
+bool isinfinity(int mult[N*N], int add[N*N], int inx) {
+    for (int i = 0; i < N; i++) {
+        if (
+            mult[i*N + inx] != inx || mult[inx*N + i] != inx
+            ||
+            add[i*N + inx] != inx || add[inx*N + i] != inx
+        ) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// -1 if not exist
+int infinity(int mult[N*N], int add[N*N]) {
+    for (int i = 0; i < N; i++) {
+        if (isinfinity(mult, add, i)) {
+            return i;
+        }
+    }
+    return -1;
+}
--- table_check.h
+++ table_check.h
@@ -12,4 +12,10 @@ bool isdistributive_left(int mult[N*N], int add[N*N]);
 bool isdistributive_right(int mult[N*N], int add[N*N]);
 bool isdistributive(int mult[N*N], int add[N*N]);
 
+bool ismono(int mult[N*N], int add[N*N]);
+bool isconst(int table[N*N]);
+int neutral(int table[N*N]);
+int zero(int mult[N*N], int add[N*N]);
+int infinity(int mult[N*N], int add[N*N]);
+
 #endif // !TABLE_CHECK_H
