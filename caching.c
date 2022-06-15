d79123c76b8d0a0ab8ef2cdfd5c5ba12cab8d216
romamihalich <miheev.roma90@mail.ru>
2022-06-15 19:46:32 +0300

    remove useless comments

--- caching.c
+++ caching.c
@@ -11,7 +11,6 @@ void cache_table_list(char* filename, List* tables, bool append) {
     } else {
         fptr = fopen(filename, "wb");
     }
-    // fwrite(&tables->count, sizeof(int), 1, fptr);
     struct Node* temp = tables->head;
     while (temp != NULL) {
         cache_table(fptr, temp->value);
@@ -22,8 +21,6 @@ void cache_table_list(char* filename, List* tables, bool append) {
 
 void read_table_list_cache(List* list, char* filename) {
     FILE* fptr = fopen(filename, "rb");
-    // int count = 0;
-    // fread(&count, sizeof(int), 1, fptr);
     while (true) {
         int* table = malloc(sizeof(int)*N*N);
         if (fread(table, sizeof(int), N*N, fptr) == 0) {
@@ -37,7 +34,6 @@ void read_table_list_cache(List* list, char* filename) {
 
 void cache_semiring_list(char* filename, List* semirings) {
     FILE* fptr = fopen(filename, "wb");
-    // fwrite(&semirings->count, sizeof(int), 1, fptr);
     struct Node* temp = semirings->head;
     while (temp != NULL) {
         Semiring* semiring = temp->value;
@@ -50,8 +46,6 @@ void cache_semiring_list(char* filename, List* semirings) {
 
 void read_semiring_list_cache(List* list, char* filename) {
     FILE* fptr = fopen(filename, "rb");
-    // int count = 0;
-    // fread(&count, sizeof(int), 1, fptr);
     while (true) {
         Semiring* semiring = malloc(sizeof(Semiring));
         semiring->add = malloc(sizeof(int)*N*N);
